d05f580edead3a25605197f42533c7b90a4cad33
Fabricio A. S. Sanches <fsanchesbr001@gmail.com>
2025-08-12 03:59:45 -0300

    Versão 1.0.9

--- src/app/components/tela-principal/tela-principal.component.ts
+++ src/app/components/tela-principal/tela-principal.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import {MatSidenavContainer, MatSidenavContent, MatSidenavModule} from '@angular/material/sidenav';
 import {MenuPrincipalComponent} from '../menu-principal/menu-principal.component';
-import {FormularioUsuariosComponent} from '../usuarios/formulario-usuarios/formulario-usuarios.component';
+import {ListaUsuariosComponent} from '../usuarios/lista-usuarios/lista-usuarios.component';
 
 @Component({
   selector: 'app-tela-principal',
-  imports: [MatSidenavContainer, MatSidenavContent, MatSidenavModule, MenuPrincipalComponent, FormularioUsuariosComponent],
+  imports: [MatSidenavContainer,
+    MatSidenavContent,
+    MatSidenavModule,
+    MenuPrincipalComponent,  ListaUsuariosComponent],
   templateUrl: './tela-principal.component.html',
   styleUrl: './tela-principal.component.css'
 })
